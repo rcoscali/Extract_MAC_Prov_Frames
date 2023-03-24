df884ff288c7aa50f0789e1a397c1fcf6348e4c0
Rémi COHEN SCALI <remi@cohenscali.net>
2023-03-24 14:37:48 +0100

    Javascript for JQueryUI widget Spinner
    Added instanciation for the JQuery-UI spinner

---
+++ public/javascripts/spinner.js
@@ -0,0 +1,13 @@
+$( ".selector" ).spinner({
+    classes:
+    {
+        "ui-spinner": "highlight"
+    },
+    culture: fr,
+    max: 1000,
+    min: 1,
+    page: 100,
+    step: 10
+});
+
+$( "#frameid" ).spinner();
